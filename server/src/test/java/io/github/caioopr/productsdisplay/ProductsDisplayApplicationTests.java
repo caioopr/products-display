e99ae87a8715d8f2a4713215455450c8f98aa50b
Caio Prado <caioppubli@gmail.com>
2023-07-11 11:22:19 -0300

    base client and server

---
+++ server/src/test/java/io/github/caioopr/productsdisplay/ProductsDisplayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.caioopr.productsdisplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsDisplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
